fbe0b162f4bc11050004917cc045ed8d8087a8c8
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-03-22 15:32:17 +0900

    ✨ :: objc, coredata 추가

---
+++ LetsEatingTime/LetsEatingTime/Obct/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  LetsEatingTime
+//
+//  Created by 최시훈 on 2023/03/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static var mainColor = UIColor(red: 10.0 / 255, green: 0.0 / 255, blue: 114.0 / 255, alpha: 1)
+    static var secondColor = UIColor(red: 220.0/255, green: 220.0/255, blue: 220.0/255, alpha: 1)
+}
+
