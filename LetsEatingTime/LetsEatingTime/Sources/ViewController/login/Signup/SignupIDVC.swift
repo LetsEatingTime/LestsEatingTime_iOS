af90a06d5a32c1a69b6e4fc617eebe5d263e6b15
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-03-25 22:45:24 +0900

    ✨ :: SignupIDVC label, TextField구현

---
+++ LetsEatingTime/LetsEatingTime/Sources/ViewController/login/Signup/SignupIDVC.swift
@@ -0,0 +1,58 @@
+//
+//  SignupIDVC.swift
+//  LetsEatingTime
+//
+//  Created by 최시훈 on 2023/03/23.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class SignupIDVC: UIViewController {
+    
+    let idLabel = UILabel().then {
+        $0.text = "아이디"
+        $0.font = .systemFont(ofSize: 30, weight: .bold)
+    }
+    let idTextField = UITextField().then {
+        $0.placeholder = "이름를 입력해주세요"
+        $0.font = .systemFont(ofSize:14.0, weight: . medium)
+        $0.autocapitalizationType = .none
+        $0.backgroundColor = .clear
+        $0.layer.cornerRadius = 15
+    }
+    let line = UIView().then {
+        $0.backgroundColor = .black
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    func setup() {
+        [   idLabel,
+            idTextField,
+            line
+        ].forEach{ self.view.addSubview($0) }
+        idLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(105)
+            $0.bottom.equalTo(idLabel.snp.top).offset(50)
+            $0.left.equalToSuperview().offset(30)
+            $0.right.equalToSuperview().offset(-70)
+        }
+        idTextField.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(190)
+            $0.bottom.equalTo(idTextField.snp.top).offset(30)
+            $0.left.equalToSuperview().offset(70)
+            $0.right.equalToSuperview().offset(-70)
+        }
+        line.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom).offset(1)
+            $0.bottom.equalTo(line.snp.top).offset(0.7)
+            $0.left.equalToSuperview().offset(70)
+            $0.right.equalToSuperview().offset(-70)
+            
+        }
+    }
+}
