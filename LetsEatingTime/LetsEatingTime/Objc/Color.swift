6fc402d6cdcc019eaca7142eb29b3c305dbe0db1
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-03-25 12:10:42 +0900

    📄 :: ColorExtention추가

--- LetsEatingTime/LetsEatingTime/Objc/Color.swift
+++ LetsEatingTime/LetsEatingTime/Objc/Color.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIColor {
-    static var mainColor = UIColor(red: 10.0 / 255, green: 0.0 / 255, blue: 114.0 / 255, alpha: 1)
+    static var mainColor = UIColor(red: 99/255, green: 99/255, blue: 99/255, alpha: 1)
     static var secondColor = UIColor(red: 220.0/255, green: 220.0/255, blue: 220.0/255, alpha: 1)
 }
 
