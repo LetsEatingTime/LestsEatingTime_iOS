2feddfb4c319d2029686c9501953d61b9671fc8f
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-03-27 23:47:42 +0900

    ✨ :: prograssView animation 추가

--- LetsEatingTime/LetsEatingTime/Sources/ViewController/login/Signup/SignupVC.swift
+++ LetsEatingTime/LetsEatingTime/Sources/ViewController/login/Signup/SignupVC.swift
@@ -14,17 +14,16 @@ import Alamofire
 class SignupVC: UIViewController {
     let logo = UILabel().then {
         $0.text = "asdf"
-        $0.font = UIFont(name: "GangwonEduAll-OTFBold", size:100)
+        $0.font = .systemFont(ofSize: 100)
         $0.textAlignment = .center
     }
     let uiView = UIView().then {
         $0.backgroundColor = .white
     }
-    let prograssView = UIProgressView().then {
+    let progressView = UIProgressView().then {
         $0.progress = 0.25
         $0.trackTintColor = .mainColor
         $0.progressTintColor = .lightGray
-        
     }
     let signupNextButton = UIButton().then {
         $0.backgroundColor = .mainColor
@@ -57,68 +56,47 @@ class SignupVC: UIViewController {
         view.backgroundColor = .white
         setup()
         configureUIView()
-        
+        UIView.animate(withDuration: 1.0) {
+            self.progressView.setProgress(0.25, animated: true)
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
 }
 extension SignupVC {
-    func logViewHierarchy(_ view: UIView? = UIApplication.shared.keyWindow) {
-        guard let view = view else {
-            return
-        }
-        
-        print(view)
-
-        for subview in view.subviews {
-            logViewHierarchy(subview)
-        }
-    }
     @objc func didTabsignupNextButton() {
-//        logViewHierarchy()
+        //        logViewHierarchy()
         switch children.first {
         case idVC:
             addChild(pwVC)
             self.uiView.addSubview(myPWView)
             idVC.removeFromParent()
             myIDView.removeFromSuperview()
-            prograssView.progress = 0.5
+            UIView.animate(withDuration: 1.0) {
+                self.progressView.setProgress(0.5, animated: true)
+            }
         case pwVC:
             addChild(nameVC)
             self.uiView.addSubview(myNameView)
             pwVC.removeFromParent()
             myPWView.removeFromSuperview()
-            prograssView.progress = 0.75
+            UIView.animate(withDuration: 1.0) {
+                self.progressView.setProgress(0.75, animated: true)
+            }
         case nameVC:
             addChild(studentNumberVC)
             self.uiView.addSubview(myStudentNumberView)
             nameVC.removeFromParent()
             myNameView.removeFromSuperview()
-            prograssView.progress = 1
+            UIView.animate(withDuration: 1.0) {
+                self.progressView.setProgress(1, animated: true)
+            }
         case studentNumberVC:
-            self.dismiss(animated: true)
+            sendInfomationToServer()
         default:
             print("The view is in an unrecognized view controller.")
         }
-        /*
-         AF.request("\(api)/user/signup.do",
-         method: .post,
-         parameters: ["email": id,
-         "name": name,
-         "password": pw],
-         encoding : JSONEncoding.default,
-         headers: ["Content-Type": "application/json"]
-         )
-         .validate()
-         .responseData { response in
-         switch response.result {
-         case.success:
-         self.dismiss(animated: true)
-         case.failure(let error):
-         print("통신 오류!\nCode:\(error._code), Message: \(error.errorDescription!)")
-         }
-         }         */
     }
     
     func configureUIView() {
@@ -130,7 +108,7 @@ extension SignupVC {
             eatingFoodImage,
             logo,
             uiView,
-            prograssView,
+            progressView,
             signupNextButton
         ].forEach{ self.view.addSubview($0) }
         logo.snp.makeConstraints {
@@ -151,8 +129,8 @@ extension SignupVC {
             $0.left.equalToSuperview().offset(40)
             $0.right.equalToSuperview().offset(-40)
         }
-        prograssView.snp.makeConstraints {
-            $0.top.equalTo(prograssView.snp.bottom).offset(-5)
+        progressView.snp.makeConstraints {
+            $0.top.equalTo(progressView.snp.bottom).offset(-5)
             $0.bottom.equalTo(signupNextButton.snp.top).offset(-10)
             $0.left.equalToSuperview().offset(45)
             $0.right.equalToSuperview().offset(-45)
@@ -162,6 +140,33 @@ extension SignupVC {
             $0.bottom.equalToSuperview().offset(-125)
             $0.left.equalToSuperview().offset(0)
             $0.right.equalToSuperview().offset(20)
+        }
+    }
+}
+extension SignupVC {
+    func sendInfomationToServer() {
+        let id = idVC.idTextField.text!
+        let pw = pwVC.pwTextField.text!
+        let name = nameVC.nameTextField.text!
+        let studentNumber = studentNumberVC.studentNumberTextField.text!
+        print(id, pw, name, studentNumber)
+        AF.request("\(api)/user/signup.do",
+                   method: .post,
+                   parameters: ["id": id,
+                                "pw": pw,
+                                "name": name,
+                                "studentNumber": studentNumber],
+                   encoding : JSONEncoding.default,
+                   headers: ["Content-Type": "application/json"]
+        )
+        .validate()
+        .responseData { response in
+            switch response.result {
+            case.success:
+                self.dismiss(animated: true)
+            case.failure(let error):
+                print("통신 오류!\nCode:\(error._code), Message: \(error.errorDescription!)")
+            }
         }
     }
 }
