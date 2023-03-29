ee938c8aa080a4f5611c1eca60b54e31ef12313d
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-03-29 23:53:47 +0900

    ✨ :: mobal sheet 구현중

--- LetsEatingTime/LetsEatingTime/Sources/View/Student/StudentIDCardVC.swift
+++ LetsEatingTime/LetsEatingTime/Sources/View/Student/StudentIDCardVC.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 
 class StudentIDCardVC: UIViewController {
+    let contentView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
+        present()
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.3) {
+            self.contentView.snp.updateConstraints { make in
+                make.height.equalTo(200)
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+    
     
 }
 
+extension StudentIDCardVC {
+    func present() {
+        [
+            contentView
+        ].forEach{ self.view.addSubview($0) }
+        contentView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(560)
+            $0.left.right.equalToSuperview().offset(0)
+            $0.bottom.equalToSuperview().offset(0)
+        }
+    }
+    
+}
