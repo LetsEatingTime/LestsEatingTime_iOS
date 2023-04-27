9422fdb10d9fddc5c500fa756ae7166cdcf85300
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-04-27 23:35:40 +0900

    ✨ :: 회원가입 모델 생성

---
+++ LetsEatingTime/LetsEatingTime/Sources/Mobel/SignupData.swift
@@ -0,0 +1,19 @@
+//
+//  SignupData.swift
+//  LetsEatingTime
+//
+//  Created by 최시훈 on 2023/04/18.
+//
+
+import Foundation
+
+struct SignupData: Decodable, Hashable {
+    let status: Int
+    let message: String
+    let data: LoginDatas
+}
+
+struct SignupDatas: Decodable, Hashable {
+    let token: String
+}
+
