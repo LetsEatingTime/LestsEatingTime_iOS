36f6e2f9478950c25213cb4dd4a423722a961307
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-05-26 09:01:51 +0900

    ✨ :: UUID 가공해서 NFC tag로 내보내기

--- LetsEatingTime/LetsEatingTime/Sources/View/Student/StudentIDCardVC.swift
+++ LetsEatingTime/LetsEatingTime/Sources/View/Student/StudentIDCardVC.swift
@@ -13,24 +13,22 @@ import CoreNFC
 import os
 
 class StudentIDCardVC: UIViewController {
-    var ndefNFCMessage: NFCNDEFMessage?
-    func gang() {
-        let uuidPayload = NFCNDEFPayload.init(format: .nfcWellKnown, type: Data([0x55]), identifier: Data(), payload: UUID().uuidString.data(using: .utf8)!)
-        let uuidMessage = NFCNDEFMessage(records: [uuidPayload])
-        showAlert(title: "쓰기 시작합니다", message: "시작합니다")
-        print("write시작한디??")
-        writeNDEF(uuidMessage) { error in
-            if error != nil {
-                print("Error writing UUID to tag: \(error!.localizedDescription)")
-                showAlert(title: "실패다 임마ㅋ", message: "ㅠㅠ")
-            } else {
-                showAlert(title: "성공이다 임마ㅋ 수고했다", message: "성공이다ㅎㅎ")
-                print("됐다 시불")
-            }
-        }
-    }
-    var readerSession: NFCNDEFReaderSession?
-    var resultPayload: Array<NFCNDEFPayload> = []
+    var session: NFCReaderSession?
+    
+    let uuid = UUID()
+
+    lazy var uuidData: Data = {
+        return uuid.uuidString.data(using: .utf8)!
+    }()
+
+    lazy var payload: NFCNDEFPayload = {
+        return NFCNDEFPayload(format: .nfcWellKnown, type: "T".data(using: .utf8)!, identifier: Data(), payload: uuidData)
+    }()
+
+    lazy var message: NFCNDEFMessage = {
+        return NFCNDEFMessage(records: [payload])
+    }()
+
     let studentShowMealsVC = StudentShowMealsVC()
     var studentShowMealsView: UIView {
         self.studentShowMealsVC.view
@@ -40,10 +38,10 @@ class StudentIDCardVC: UIViewController {
     let studentIDCard = UIImageView().then {
         $0.image = UIImage(named: "studentIDCard")
     }
-//    let mealslabel = UILabel().then {
-//        $0.text = "급식이 없습니다"
-//        $0.
-//    }
+    //    let mealslabel = UILabel().then {
+    //        $0.text = "급식이 없습니다"
+    //        $0.
+    //    }
     let mealsButton = UIButton().then {
         $0.backgroundColor = .clear
         $0.addTarget(self, action: #selector(didPressMealsButton), for: .touchUpInside)
@@ -74,6 +72,7 @@ class StudentIDCardVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setup()
+        print("NFCNDEFMessage: \(message)")
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -115,8 +114,8 @@ extension StudentIDCardVC {
             $0.bottom.equalTo(studentIDCard.snp.top).offset(547)
         }
         [
-//            mealsImage,
-//            mealsLabel,
+            //            mealsImage,
+            //            mealsLabel,
             mealsView,
             mealsButton,
             infomationChangeButton,
@@ -161,90 +160,171 @@ extension StudentIDCardVC {
         }
     }
 }
-extension StudentIDCardVC: NFCNDEFTag {
-    var isAvailable: Bool { //태그가 연결되어 있고 NDEF를 지원하는지 확인
-        return true
-    }
-    // Query the NDEF status of the tag
-    func queryNDEFStatus(completionHandler: @escaping (NFCNDEFStatus, Int, Error?) -> Void) {  //태그의 NDEF 상태를 쿼리하는 자체 로직을 구현
-        completionHandler(.readWrite, 1024, nil)//.readWrite 상태 및 1024 용량을 반환하는 코드
-    }
-    // Read the NDEF message from the tag
-    func readNDEF(completionHandler: @escaping (NFCNDEFMessage?, Error?) -> Void) {  //UUID를 페이로드로 사용하여 사용자 지정 NDEF 메시지를 생성하는 코드
-        completionHandler(ndefNFCMessage, nil)
-        print("UUIDSuccess")
-    }
-    func writeNDEF(_ ndefMessage: NFCNDEFMessage, completionHandler: @escaping (Error?) -> Void) { //NDEF 메시지를 쓰는 고유 로직
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            completionHandler(nil)
-        }
-    }
-    func writeLock(completionHandler: @escaping (Error?) -> Void) {//쓰기할때 tag 잠그는 코드
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            completionHandler(nil)
-        }
-    }
-    static var supportsSecureCoding: Bool { // tag가 보안코딩을 지원하는가?
-        return false
-    }
-    func copy(with zone: NSZone? = nil) -> Any { //tag 복사본
-        let copy = StudentIDCardVC()
-        return copy
-    }
+extension StudentIDCardVC: NFCNDEFReaderSessionDelegate {
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
-        print("NFC session did invalidate with error: \(error.localizedDescription)")
+        
     }
-    private func tagRemovalDetect(_ tag: NFCNDEFTag) {
-        self.readerSession?.connect(to: tag) { (error: Error?) in
-            if error != nil || !tag.isAvailable {
-                os_log("Restart polling")
-                self.readerSession?.restartPolling()
+    func gang() {
+        message = message
+        session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        session?.alertMessage = "폰을 태그에 가까이 가져가세요"
+        session?.begin()
+    }
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {}
+    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
+        var str: String = "\(message)"
+        var strToUInt: [UInt8] = [UInt8](str.utf8)
+        
+        if tags.count > 1 {
+            let retryInterval = DispatchTimeInterval.milliseconds(500)
+            session.alertMessage = "하나이상의 태그가 입력 되었습니다. 다시 실행해주세요"
+            DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval, execute: {
+                session.restartPolling()
+            })
+            return
+        }
+        
+        let tag = tags.first!
+        session.connect(to: tag, completionHandler: {(error:Error?) in
+            if nil != error {
+                session.alertMessage = "태그 연결 실패"
+                session.invalidate()
                 return
             }
-            DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + .milliseconds(500), execute: {
-                self.tagRemovalDetect(tag)
-            })
-        }
-    }
-    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
-        print("readerSession:didDetect")
-        if tags.count > 1 {
-            session.alertMessage = "태그가 두개 이상 발견되었습니다. 태그 하나만 표시해주세요."
-            self.tagRemovalDetect(tags.first!)
-        } else {
-            guard let tag = tags.first else { return }
-            session.connect(to: tag ) { (error: Error?) in
-                if error != nil {
-                    session.restartPolling()
+            tag.queryNDEFStatus(completionHandler: {(ndefStatus: NFCNDEFStatus, capacity:Int, error: Error?) in
+                guard error == nil else {
+                    session.alertMessage = "태그의 NDEF 상태를 쿼리할 수 없습니다"
+                    session.invalidate()
                     return
                 }
-                tag.queryNDEFStatus() { (status: NFCNDEFStatus, capacity: Int, error: Error?) in
-                    if error != nil {
-                        session.invalidate(errorMessage: "NDEF 상태를 확인하지 못 했습니다. 다시 시도해주세요.")
-                        return
-                    }
-                    
-                    if status == .readOnly {
-                        session.invalidate(errorMessage: "태그를 쓸 수 없습니다.")
-                    } else if status == .readWrite {
-                        if self.ndefNFCMessage!.length > capacity {
-                            session.invalidate(errorMessage: "태그 용량이 너무 작습니다.")
-                            return
-                        }
-                        tag.writeNDEF(self.ndefNFCMessage!) { (error: Error?) in
-                            if error != nil {
-                                session.invalidate(errorMessage: "태그 업데이트에 실패했습니다. 다시 시도해주세요.")
-                            } else {
-                                session.alertMessage = "업데이트 성공!"
-                                session.invalidate()
-                            }
-                        }
+                switch ndefStatus {
+                case .notSupported:
+                    session.alertMessage = "Tag is not NDEF compliant"
+                    session.invalidate()
+                case .readWrite:
+                    tag.writeNDEF(.init(records: [.init(format: .nfcWellKnown, type: Data([06]), identifier: Data([0x0c]), payload: Data(strToUInt))]), completionHandler: { (error: Error?) in
+                                    if nil != error {
+                        session.alertMessage = "쓰기가 실패했습니다 \(error!)"
                     } else {
-                        session.invalidate(errorMessage: "태그가 NDEF 형식이 아닙니다.")
+                        session.alertMessage = "쓰기 성공"
+                        print("성공")
                     }
+                })
+                case .readOnly:
+                    session.alertMessage = "Tag is readOnly"
+                    session.invalidate()
+                @unknown default:
+                    session.alertMessage = "NDEF 태그 상태가 정의되지 않았습니다"
+                    session.invalidate()
+                }
+            })
+            
+        })
+    }
+    func readerSessionDidBecomeActive(_ session: NFCNDEFReaderSession) {
+        
+    }
+    func readerSession(_ session: NFCReaderSession, didInvalidateWithError error:Error) {
+        if let readerError = error as? NFCReaderError {
+            if (readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead)
+                && (readerError.code != .readerSessionInvalidationErrorUserCanceled) {
+                let alertController = UIAlertController(
+                    title: "Session Invalidated",
+                    message: error.localizedDescription,
+                    preferredStyle: .alert
+                )
+                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                DispatchQueue.main.async {
+                    self.present(alertController, animated: true, completion: nil)
                 }
             }
         }
     }
+    
+    
+    //    var isAvailable: Bool { //태그가 연결되어 있고 NDEF를 지원하는지 확인
+    //        return true
+    //    }
+    //    // Query the NDEF status of the tag
+    //    func queryNDEFStatus(completionHandler: @escaping (NFCNDEFStatus, Int, Error?) -> Void) {  //태그의 NDEF 상태를 쿼리하는 자체 로직을 구현
+    //        completionHandler(.readWrite, 1024, nil)//.readWrite 상태 및 1024 용량을 반환하는 코드
+    //    }
+    //    // Read the NDEF message from the tag
+    //    func readNDEF(completionHandler: @escaping (NFCNDEFMessage?, Error?) -> Void) {  //UUID를 페이로드로 사용하여 사용자 지정 NDEF 메시지를 생성하는 코드
+    //        completionHandler(ndefNFCMessage, nil)
+    //        print("UUIDSuccess")
+    //    }
+    //    func writeNDEF(_ ndefMessage: NFCNDEFMessage, completionHandler: @escaping (Error?) -> Void) { //NDEF 메시지를 쓰는 고유 로직
+    //        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+    //            completionHandler(nil)
+    //        }
+    //    }
+    //    func writeLock(completionHandler: @escaping (Error?) -> Void) {//쓰기할때 tag 잠그는 코드
+    //        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+    //            completionHandler(nil)
+    //        }
+    //    }
+    //    static var supportsSecureCoding: Bool { // tag가 보안코딩을 지원하는가?
+    //        return false
+    //    }
+    //    func copy(with zone: NSZone? = nil) -> Any { //tag 복사본
+    //        let copy = StudentIDCardVC()
+    //        return copy
+    //    }
+    //    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+    //        print("NFC session did invalidate with error: \(error.localizedDescription)")
+    //    }
+    //    private func tagRemovalDetect(_ tag: NFCNDEFTag) {
+    //        self.readerSession?.connect(to: tag) { (error: Error?) in
+    //            if error != nil || !tag.isAvailable {
+    //                os_log("Restart polling")
+    //                self.readerSession?.restartPolling()
+    //                return
+    //            }
+    //            DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + .milliseconds(500), execute: {
+    //                self.tagRemovalDetect(tag)
+    //            })
+    //        }
+    //    }
+    //    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
+    //        print("readerSession:didDetect")
+    //        if tags.count > 1 {
+    //            session.alertMessage = "태그가 두개 이상 발견되었습니다. 태그 하나만 표시해주세요."
+    //            self.tagRemovalDetect(tags.first!)
+    //        } else {
+    //            guard let tag = tags.first else { return }
+    //            session.connect(to: tag ) { (error: Error?) in
+    //                if error != nil {
+    //                    session.restartPolling()
+    //                    return
+    //                }
+    //                tag.queryNDEFStatus() { (status: NFCNDEFStatus, capacity: Int, error: Error?) in
+    //                    if error != nil {
+    //                        session.invalidate(errorMessage: "NDEF 상태를 확인하지 못 했습니다. 다시 시도해주세요.")
+    //                        return
+    //                    }
+    //
+    //                    if status == .readOnly {
+    //                        session.invalidate(errorMessage: "태그를 쓸 수 없습니다.")
+    //                    } else if status == .readWrite {
+    //                        if self.ndefNFCMessage!.length > capacity {
+    //                            session.invalidate(errorMessage: "태그 용량이 너무 작습니다.")
+    //                            return
+    //                        }
+    //                        tag.writeNDEF(self.ndefNFCMessage!) { (error: Error?) in
+    //                            if error != nil {
+    //                                session.invalidate(errorMessage: "태그 업데이트에 실패했습니다. 다시 시도해주세요.")
+    //                            } else {
+    //                                session.alertMessage = "업데이트 성공!"
+    //                                session.invalidate()
+    //                            }
+    //                        }
+    //                    } else {
+    //                        session.invalidate(errorMessage: "태그가 NDEF 형식이 아닙니다.")
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
     
 }
