7acded651e4ef7815e3fdf28739e516b6917a72f
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-07-18 19:29:16 +0900

    ✨ :: 도메인 추가

--- LetsEatingTime/LetsEatingTime/Resources/Object/ showAlert.swift
+++ LetsEatingTime/LetsEatingTime/Resources/Object/ showAlert.swift
@@ -6,11 +6,11 @@
 //
 
 import UIKit
-func showAlert(title: String, message: String) {
-    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
-    alertController.addAction(okAction)
-    if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
-        rootViewController.present(alertController, animated: true, completion: nil)
-    }
-}
+//func showAlert(title: String, message: String) {
+//    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//    let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+//    alertController.addAction(okAction)
+//    if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+//        rootViewController.present(alertController, animated: true, completion: nil)
+//    }
+//}
