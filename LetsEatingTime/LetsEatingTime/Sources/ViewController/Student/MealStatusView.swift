cb8b2f2fe5f5eda82404f598dcb9189e2290201f
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-08-13 23:52:02 +0900

    ✨ :: 급식 현황 View 구현

--- LetsEatingTime/LetsEatingTime/Sources/ViewController/Student/MealStatusView.swift
+++ LetsEatingTime/LetsEatingTime/Sources/ViewController/Student/MealStatusView.swift
@@ -11,23 +11,62 @@ import SnapKit
 import Alamofire
 
 class MealsStatusView: UIView {
+    
     let mealsExplanationLabel = UILabel().then {
         $0.text = "나의 급식 현황"
+        $0.font = .systemFont(ofSize: 15, weight: .medium)
+        $0.textAlignment = .center
     }
-    let breakfastImageView = UIImageView().then {
-        $0.image = UIImage(contentsOfFile: "Breakfast")
+    let mealsDate = UILabel().then {
+        let currentDate = Date()
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: currentDate)
+        let month = calendar.component(.month, from: currentDate)
+        let day = calendar.component(.day, from: currentDate)
+        $0.text = "\(year)년\(month)월\(day)일"
+        $0.font = .systemFont(ofSize: 15, weight: .medium)
+        $0.textAlignment = .center
     }
-    let breakfastLabel = UILabel().then {
+    let breakfast = UILabel().then {
         $0.text = "아침"
+        $0.font = .systemFont(ofSize: 15, weight: .medium)
     }
-    let lunchImageView = UIImageView().then {
-        $0.image = UIImage(contentsOfFile: "lunch")
+    let breakfastImage = UIImageView().then {
+        $0.image = UIImage(named: "Breakfast")
     }
-    let dinnerImageView = UIImageView().then {
-        $0.image = UIImage(contentsOfFile: "dinner")
+    let breakfastCKView = UIView().then {
+        $0.backgroundColor = .lightGray
+        $0.layer.cornerRadius = 5
     }
+    
+    let lunch = UILabel().then {
+        $0.text = "점심"
+        $0.font = .systemFont(ofSize: 15, weight: .medium)
+
+    }
+    let lunchImage = UIImageView().then {
+        $0.image = UIImage(named: "Lunch")
+    }
+    let lunchCKView = UIView().then {
+        $0.backgroundColor = .lightGray
+        $0.layer.cornerRadius = 5
+    }
+    let dinner = UILabel().then {
+        $0.text = "저녁"
+        $0.font = .systemFont(ofSize: 15, weight: .medium)
+    }
+    let dinnerImage = UIImageView().then {
+        $0.image = UIImage(named: "Dinner")
+    }
+    let dinnerCKView = UIView().then {
+        $0.backgroundColor = .lightGray
+        $0.layer.cornerRadius = 5
+
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setup()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -35,31 +74,72 @@ class MealsStatusView: UIView {
     func setup() {
         [
             mealsExplanationLabel,
-            breakfastImageView,
-            lunchImageView,
-            dinnerImageView
+            mealsDate,
+            breakfastImage,
+            breakfast,
+            breakfastCKView,
+            lunchImage,
+            lunch,
+            lunchCKView,
+            dinnerImage,
+            dinner,
+            dinnerCKView
         ].forEach { self.addSubview($0) }
         mealsExplanationLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().offset(20)
         }
-        breakfastImageView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(50)
-            $0.left.equalToSuperview().offset(40)
+        mealsDate.snp.makeConstraints {
+            $0.top.equalTo(mealsExplanationLabel.snp.bottom).offset(5)
+            $0.centerX.equalToSuperview()
+        }
+        breakfast.snp.makeConstraints {
+            $0.centerY.equalTo(breakfastImage)
+            $0.left.equalTo(breakfastImage.snp.right).offset(8)
+        }
+        breakfastImage.snp.makeConstraints {
+            $0.top.equalTo(mealsDate.snp.bottom).offset(10)
+            $0.left.equalToSuperview().offset(70)
             $0.width.equalTo(30)
             $0.height.equalTo(30)
         }
-        lunchImageView.snp.makeConstraints {
-            $0.top.equalTo(breakfastImageView.snp.bottom).offset(15)
-            $0.left.equalToSuperview().offset(40)
+        breakfastCKView.snp.makeConstraints {
+            $0.centerY.equalTo(breakfastImage)
+            $0.right.equalToSuperview().offset(-70)
+            $0.width.equalTo(40)
+            $0.height.equalTo(20)
+        }
+        lunch.snp.makeConstraints {
+            $0.centerY.equalTo(lunchImage)
+            $0.left.equalTo(lunchImage.snp.right).offset(8)
+        }
+        lunchImage.snp.makeConstraints {
+            $0.top.equalTo(breakfastImage.snp.bottom).offset(10)
+            $0.left.equalToSuperview().offset(70)
             $0.width.equalTo(30)
             $0.height.equalTo(30)
         }
-        dinnerImageView.snp.makeConstraints {
-            $0.top.equalTo(lunchImageView.snp.bottom).offset(15)
-            $0.left.equalToSuperview().offset(40)
+        lunchCKView.snp.makeConstraints {
+            $0.centerY.equalTo(lunchImage)
+            $0.right.equalToSuperview().offset(-70)
+            $0.width.equalTo(40)
+            $0.height.equalTo(20)
+        }
+        dinner.snp.makeConstraints {
+            $0.centerY.equalTo(dinnerImage)
+            $0.left.equalTo(dinnerImage.snp.right).offset(8)
+        }
+        dinnerImage.snp.makeConstraints {
+            $0.top.equalTo(lunchImage.snp.bottom).offset(10)
+            $0.left.equalToSuperview().offset(70)
             $0.width.equalTo(30)
             $0.height.equalTo(30)
+        }
+        dinnerCKView.snp.makeConstraints {
+            $0.centerY.equalTo(dinnerImage)
+            $0.right.equalToSuperview().offset(-70)
+            $0.width.equalTo(40)
+            $0.height.equalTo(20)
         }
     }
 }
