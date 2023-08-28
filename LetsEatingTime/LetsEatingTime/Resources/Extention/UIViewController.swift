cba3441a11de6a5d2118cf85a87d98eac9e47e9c
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-08-28 13:48:56 +0900

    ✨ :: showAlert 함수 구현

---
+++ LetsEatingTime/LetsEatingTime/Resources/Extention/UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController.swift
+//  LetsEatingTime
+//
+//  Created by 최시훈 on 2023/08/25.
+//
+
+import UIKit
+
+public extension UIViewController {
+    func showAlert(
+        title: String?,
+        message: String,
+        handler: ((UIAlertAction) -> Void)? = nil
+    ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: handler))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
