7913dbe213ad375069b7e17359aeda9d0e362148
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-12-04 23:47:38 +0900

    ✨ :: UserDefaultes에 아이디 저장하는 코드 구현

---
+++ LetsEatingTime/LetsEatingTime/Resources/Object/Identifier.swift
@@ -0,0 +1,29 @@
+//
+//  Identifier.swift
+//  LetsEatingTime
+//
+//  Created by 최시훈 on 2023/08/18.
+//
+
+import Foundation
+
+struct Id: Codable {
+    let id: String
+}
+
+enum Identifier {
+    case id
+}
+class IdManager {
+    static func save(_ Id: Identifier, _ value: String) {
+        UserDefaults.standard.set(value, forKey: String(describing: Id))
+    }
+    
+    static func get(_ Id: Identifier) -> String? {
+        return UserDefaults.standard.string(forKey: String(describing: Id))
+    }
+    
+    static func remove(_ Id: Identifier) {
+        UserDefaults.standard.removeObject(forKey: String(describing: Id))
+    }
+}
