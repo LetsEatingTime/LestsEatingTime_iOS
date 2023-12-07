8eda132b6da73afdc2ae15e698e02b1a8607d13c
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-12-07 22:19:54 +0900

    Update SceneDelegate.swift

--- LetsEatingTime/LetsEatingTime/SceneDelegate.swift
+++ LetsEatingTime/LetsEatingTime/SceneDelegate.swift
@@ -14,7 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = SigninVC()
+        let mainViewController = SigninVC()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
