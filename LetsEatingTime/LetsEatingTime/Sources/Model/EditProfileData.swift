30704b1204d605518c8d34dc048aa4f20795c370
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-12-07 22:20:00 +0900

    Create EditProfileData.swift

---
+++ LetsEatingTime/LetsEatingTime/Sources/Model/EditProfileData.swift
@@ -0,0 +1,16 @@
+//
+//  EditProfile.swift
+//  LetsEatingTime
+//
+//  Created by 최시훈 on 12/7/23.
+//
+
+import Foundation
+
+struct EditProfileData: Decodable {
+    let id: String
+    let name: String
+    let grade: String
+    let className: String
+    let classNo: String
+}
